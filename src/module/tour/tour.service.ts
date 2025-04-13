ce83cb82ab6405e9d654da483fd55fd09e0005ad
AnasCoding1986 <anassust.scw@gmail.com>
2025-04-13 09:26:49 +0600

    sort completed

--- src/module/tour/tour.service.ts
+++ src/module/tour/tour.service.ts
@@ -7,17 +7,44 @@ const createTour = async (payLoad: ITour) => {
   return result
 }
 
-const getTour = async (query: Record<string, unknown>) => {
-  console.log(query)
-  const searchTerm = (query?.searchTerm as string) || ''
+const getTour = async (query: Record<string, unknown>): Promise<ITour[]> => {
+  const queryObj = { ...query }
+  const excludingItems = ['searchTerm', 'page', 'limit', 'sortBy', 'sortOrder']
+  excludingItems.forEach((item) => delete queryObj[item])
 
+  const searchTerm = (query?.searchTerm as string) || ''
   const searchAbleFields = ['name', 'location']
 
-  const result = await Tour.find({
-    $or: searchAbleFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: 'i' },
-    })),
-  })
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const andConditions: any[] = []
+
+  if (searchTerm) {
+    andConditions.push({
+      $or: searchAbleFields.map((field) => ({
+        [field]: { $regex: searchTerm, $options: 'i' },
+      })),
+    })
+  }
+
+  if (Object.keys(queryObj).length) {
+    andConditions.push(queryObj)
+  }
+
+  const finalQuery = andConditions.length > 0 ? { $and: andConditions } : {}
+
+  const page = Number(query.page) || 1
+  const limit = Number(query.limit) || 10
+  const skip = (page - 1) * limit
+
+  let sortStr = ''
+  if (query?.sortBy && query?.sortOrder) {
+    sortStr = `${query.sortOrder === 'desc' ? '-' : ''}${query.sortBy}`
+  }
+
+  const result = await Tour.find(finalQuery)
+    .skip(skip)
+    .limit(limit)
+    .sort(sortStr || '')
 
   return result
 }
